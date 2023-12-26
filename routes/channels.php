d8ae70d8324b4fddfa3ecd679127b5fbbc01ae28
e.muchasaj <e.muchasaj@7btc.co>
2023-12-26 09:42:00 +0100

    no message

--- routes/channels.php
+++ routes/channels.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Broadcast;
 | Broadcast Channels
 |--------------------------------------------------------------------------
 |
-| Here you may register all of the event broadcasting channels that your
+| Here you may register all the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
 |
@@ -15,4 +15,11 @@ use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('chat-channel', function() {
+    if (auth()->check()) {
+        return true;
+    }
+    return false;
 });
